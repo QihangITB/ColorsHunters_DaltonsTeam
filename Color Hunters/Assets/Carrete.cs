9a062ee16cf6c4a456d1a95bcf17c72924e871b7
TopTopsEnric <enricguillotiblanco@gmail.com>
2024-11-29 17:47:09 +0100

    puxewar

--- Color Hunters/Assets/Carrete.cs
+++ Color Hunters/Assets/Carrete.cs
@@ -9,121 +9,76 @@ using TMPro;
 
 public class Carrete : MonoBehaviour
 {
-    public List<string> carrete = GameManager.gameManager.carrete;
-    public List<int> imagen = new List<int>();
-    public List<int> posicion = new List<int>();
+    
     string[] imagene_1;
-    string[] imagene_2;
-    string[] imagene_3;
-    string[] imagene_4;
-    string[] imagene_5;
-    string[] imagene_6;
-    string[] imagene_7;
-    string[] imagene_8;
-   
+
     private void Awake()
     {
-        imagene_1 = new string[9] { "bosque esquina superior", "imagen_1_2", "imagen_1_3", "imagen_1_4", "imagen_1_5", "imagen_1_6", "imagen_1_7", "imagen_1_8", "imagen_1_9" };
-        imagene_2 = new string[9] { "imagen_2_1", "imagen_2_2", "imagen_2_3", "imagen_2_4", "imagen_2_5", "imagen_2_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
-        imagene_3 = new string[9] { "imagen_3_1", "imagen_3_2", "imagen_3_3", "imagen_3_4", "imagen_2_5", "imagen_2_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
-        imagene_4 = new string[9] { "imagen_3_1", "imagen_3_2", "imagen_3_3", "imagen_3_4", "imagen_4_5", "imagen_4_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
-        imagene_5 = new string[9] { "imagen_5_1", "imagen_5_2", "imagen_3_3", "imagen_3_4", "imagen_2_5", "imagen_2_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
-        imagene_6 = new string[9] { "imagen_6_1", "imagen_5_2", "imagen_3_3", "imagen_3_4", "imagen_4_5", "imagen_4_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
-        imagene_7 = new string[9] { "imagen_7_1", "imagen_7_2", "imagen_3_3", "imagen_3_4", "imagen_7_5", "imagen_7_6", "imagen_7_7", "imagen_7_8", "imagen_7_9" };
-        imagene_8 = new string[9] { "imagen_8_1", "imagen_8_2", "imagen_3_3", "imagen_3_4", "imagen_4_5", "imagen_8_6", "imagen_2_7", "imagen_2_8", "imagen_2_9" };
+        imagene_1 = new string[9] { "flores", "Castillo", "sumama", "alicante", "picante", "imagen_1_6", "imagen_1_7", "imagen_1_8", "imagen_1_9" };
+       // Debug.Log("Estado del carrete en el GameManager antes de llamar a rellenar(): " + string.Join(", ", GameManager.gameManager.carrete));
         rellenar();
+
     }
 
     public void rellenar()
     {
-        if (carrete.Count >= 5)
+        int nuevaImagen = GameManager.gameManager.getImagen();
+        Debug.Log("el numero de carga del acrrete es: " + GameManager.gameManager.carrete.Count);
+        foreach (var car in GameManager.gameManager.carrete)
         {
-            carrete.RemoveAt(0);
-            posicion.RemoveAt(0);
-            imagen.RemoveAt(0);
-            carrete.Add(seleccion(GameManager.gameManager.getImagen(), GameManager.gameManager.getImagen_imagen_guadricula()));
-            imagen.Add(GameManager.gameManager.getImagen());
-            posicion.Add(GameManager.gameManager.getImagen_imagen_guadricula());
+            Debug.Log("EN EL CARRETE HAY " + car);
         }
-        else
+        // Asegurar el tamaño máximo del carrete
+        if (GameManager.gameManager.carrete.Count >= 8)
         {
-            carrete.Add(seleccion(GameManager.gameManager.getImagen(), GameManager.gameManager.getImagen_imagen_guadricula()));
+            GameManager.gameManager.carrete.RemoveAt(0); // Eliminar el primero
+            GameManager.gameManager.posiciones.RemoveAt(0);  // Eliminar el primero
         }
 
-        for (int i = 0; i < 5; i++)
+        // Agregar nuevas entradas
+
+        Debug.Log(" int nuevaimagen " + nuevaImagen);
+        GameManager.gameManager.carrete.Add(seleccion(nuevaImagen));
+        //Debug.Log("Después de modificar: " + string.Join(", ", GameManager.gameManager.carrete));
+        GameManager.gameManager.posiciones.Add(nuevaImagen);
+
+        // Actualizar la UI
+        for (int i = 0; i < 8; i++)
         {
-            // Verificar si el hijo existe
-            if (i >= transform.childCount)
-            {
-                Debug.LogWarning($"El objeto transform no tiene suficiente hijos. Se esperaba al menos 5, pero tiene {transform.childCount}.");
-                break;
-            }
+            if (i >= transform.childCount) break;
 
             Transform currentChild = transform.GetChild(i);
             Transform grandchild = currentChild.GetChild(0);
             TextMeshProUGUI tmpComponent = grandchild.GetComponent<TextMeshProUGUI>();
 
-            // Verificar si el componente existe
             if (tmpComponent == null)
             {
                 Debug.LogWarning($"El hijo {i} no tiene un componente TextMeshProUGUI.");
                 continue;
             }
 
-            // Verificar si carrete tiene suficiente elementos
-            if (i >= carrete.Count)
+            if (i >= GameManager.gameManager.carrete.Count)
             {
+               // Debug.Log("se ha puesto empty");
                 tmpComponent.text = "Empty";
-                Debug.Log($"No hay suficiente elementos en carrete para llenar el hijo {i}. Se asigna 'Empty'.");
             }
             else
             {
-                tmpComponent.text = carrete[i];
+                tmpComponent.text = GameManager.gameManager.carrete[i];
             }
         }
     }
 
-    public string seleccion(int numeroArray, int numeroPosicion)
+    public string seleccion(int numeroArray)
     {
         string anyadir = "";
-                switch (numeroArray)
-                {
-                    case 0:
-                        Debug.Log("se ha impreso la foto");
-                         anyadir= imagene_1[numeroPosicion];
-                        break;
-                    case 1:
-                        Debug.Log("se ha impreso la foto");
-                              anyadir = imagene_2[numeroPosicion];
-                        break;
-                    case 2:
-                        Debug.Log("se ha impreso la foto");
-                              anyadir = imagene_3[numeroPosicion];
-                        break;
-                    case 3:
-                        Debug.Log("se ha impreso la foto");
-                             anyadir = imagene_4[numeroPosicion];
-                        break;
-                    case 4:
-                        Debug.Log("se ha impreso la foto");
-                             anyadir = imagene_5[numeroPosicion];
-                        break;
-                    case 5:
-                        Debug.Log("se ha impreso la foto");
-                             anyadir = imagene_6[numeroPosicion];
-                        break;
-                    case 6:
-                        Debug.Log("se ha impreso la foto");
-                             anyadir = imagene_7[numeroPosicion];
-                        break;
-                    case 7:
-                        Debug.Log("se ha impreso la foto");
-                            anyadir = imagene_8[numeroPosicion];
-                        break;
-                }
+        anyadir = imagene_1[numeroArray];
         return anyadir;
-            
+
     }
 
 
 }
+
+
+
