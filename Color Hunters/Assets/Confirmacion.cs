b23b8f93ec12bb8f60864b756283a186111c52ec
QihangITB <qihang.li.7e7@itb.cat>
2024-12-01 03:03:54 +0100

    más limpieza

--- Color Hunters/Assets/Confirmacion.cs
+++ Color Hunters/Assets/Confirmacion.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Confirmacion : MonoBehaviour
 {
-    // Start is called before the first frame update
    public void sala_de_datos()
     {
         // comprueba que todo este bien y entonces te envia a la sala de los datos, el numero es temporal, cambiar a gusto
--- Color Hunters/Assets/enviar_cuadicula.cs
+++ Color Hunters/Assets/enviar_cuadicula.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enviar_guadricula : MonoBehaviour
+public class enviar_cuadricula : MonoBehaviour
 {
     
      // envia la posicion a la camara
      public void enviar_posicion(int posicion)
     {
-
         GameManager.gameManager.SetCuadricula(posicion);
         Debug.Log("se esta enviando la posicion");
     }
